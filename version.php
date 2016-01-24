ba7ea8a583fed3cc69dc0fa99dd9087c3a425a3a
scottmetcalf <scott.metcalf@fresno.edu>
2016-01-23 22:15:30 -0800

    Initial commit

---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Version details.
+ *
+ * @package    local_fputils
+ * @copyright
+ * @license
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2015100200;
+$plugin->requires  = 2014110400;        // Requires this Moodle version
+$plugin->cron      = 0;
+$plugin->component = 'local_overdrive';
+$plugin->release   = '1.0';
